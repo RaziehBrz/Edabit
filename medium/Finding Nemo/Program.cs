3fa84e4357c94511710cafd27d1a69da1fbeae58
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-31 21:30:56 +0330

    ...

---
+++ medium/Finding Nemo/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    public static string FindNemo(string sentence)
+    {
+        var myArray = new string[] { };
+        myArray = sentence.Split(' ');
+        if (myArray.Contains("Nemo"))
+        {
+            var index = Array.FindIndex(myArray, x => x == "Nemo") + 1;
+            return $"I found Nemo at {index}!";
+        }
+        return "I can't find Nemo :(";
+    }
+}
