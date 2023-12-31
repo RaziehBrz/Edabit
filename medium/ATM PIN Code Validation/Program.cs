e4d0daf1e66d477bd82f3aa312cd9b7e5055da06
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-31 10:08:22 +0330

    ...

--- medium/ATM PIN Code Validation/Program.cs
+++ medium/ATM PIN Code Validation/Program.cs
@@ -7,20 +7,6 @@
     }
     static bool ValidationPIN(string pin)
     {
-        bool isDigit = false;
-        foreach (var x in pin)
-        {
-            if (char.IsDigit(x)) isDigit = true;
-            else
-            {
-                isDigit = false;
-                break;
-            }
-        }
-        if ((pin.Length == 4 || pin.Length == 6) && isDigit)
-        {
-            return true;
-        }
-        return false;
+        return (pin.Length == 4 || pin.Length == 6) && pin.All(Char.IsDigit);
     }
 }
