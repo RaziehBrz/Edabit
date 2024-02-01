028892b31a7a4238f1311254f12b80f0a4390e69
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-01 10:26:20 +0330

    ...

---
+++ medium/Which Generation Are You/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Edabit
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static string generation(int x, char y)
+        {
+            switch (x)
+            {
+                case -3:
+                    return y == 'f' ? "great grandmother" : "great grandfather";
+                case -2:
+                    return y == 'f' ? "grandmother" : "grandfather";
+                case -1:
+                    return y == 'f' ? "mother" : "father";
+                case 0:
+                    return "me!";
+                case 1:
+                    return y == 'f' ? "daughter" : "son";
+                case 2:
+                    return y == 'f' ? "granddaughter" : "grandson";
+                case 3:
+                    return y == 'f' ? "great granddaughter" : "great grandson";
+                default:
+                    return "";
+            }
+        }
+    }
+
+}
