28cf1750b112fc5e5a557ba91feca65650827f30
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-02 15:28:38 +0330

    ...

---
+++ medium/Fraction Greater Than One/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var result = GreaterThanOne("10/10");
+        Console.WriteLine(result);
+    }
+    static bool GreaterThanOne(string str)
+    {
+        return int.Parse(str.Split('/')[0]) > int.Parse(str.Split('/')[1]);
+    }
+}
+
