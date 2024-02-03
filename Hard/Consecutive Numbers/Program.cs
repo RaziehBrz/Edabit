d7ac50d2c015b7d7994d12f0cd9a001db5c1239a
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-03 10:56:34 +0330

    ...

---
+++ Hard/Consecutive Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    static bool Cons(int[] arr)
+    {
+        Array.Sort(arr);
+        int counter = 1;
+        var min = arr.Min();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i != 0 && arr[i] == min + i) counter++;
+        }
+        return counter == arr.Length;
+    }
+}
