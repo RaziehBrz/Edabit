0ef692c65b5103f1bcb1c10b28222a9f065a3aeb
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-05 16:37:05 +0330

    ...

---
+++ medium/Return the Index of All Capital Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿class Progarm
+{
+    static void Main(string[] args)
+    {
+    }
+    static int[] IndexOfCapitals(string str)
+    {
+        var listOfIndex = new List<int>();
+        foreach (var c in str)
+        {
+            if (char.IsUpper(c)) listOfIndex.Add(str.IndexOf(c));
+        }
+        return listOfIndex.ToArray();
+    }
+}
