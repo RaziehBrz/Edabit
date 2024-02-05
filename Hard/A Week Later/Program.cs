53627dd2b6dea7d9065b6dfd42d30ae077e5679a
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-05 16:55:25 +0330

    ...

---
+++ Hard/A Week Later/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(WeekAfter("21/12/2023"));
+
+    }
+    static string WeekAfter(string date)
+    {
+        var x = date.Split('/');
+        var day = x[0];
+        if (day.StartsWith("0"))
+        {
+            day = (int.Parse(day) + 7).ToString();
+        }
+        else day = (int.Parse(day) + 7).ToString();
+
+        return day + date.Substring(2);
+    }
+}
