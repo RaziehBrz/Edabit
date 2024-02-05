ab33e82c163ba932f11232a3d9a7729dec5f462e
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-05 16:47:36 +0330

    ...

---
+++ medium/Find the Bomb/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Bomb("bOmb"));
+
+    }
+    static string Bomb(string str)
+    {
+        return str.ToLower().Contains("bomb") ? "Duck!!!" : "There is no bomb, relax.";
+    }
+
+}
