c121c8bca6dfe53ab17905ebaabcf0d62762445e
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-07 15:15:43 +0330

    ...

---
+++ medium/Find the Characters Counterpart Char Code/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    static int CounterpartCharCode(char symbol)
+    {
+        return char.IsLower(symbol) ? char.ToUpper(symbol) : char.ToLower(symbol);
+    }
+}
