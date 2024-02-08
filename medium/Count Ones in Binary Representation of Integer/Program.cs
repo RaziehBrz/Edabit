34b8762caf9e90eea3fd41f74801da092aac0a86
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-08 17:09:28 +0330

    ...

---
+++ medium/Count Ones in Binary Representation of Integer/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    static int CountOne(int i)
+    {
+        string result = "";
+        do
+        {
+            result += (i % 2).ToString();
+            i /= 2;
+        } while (i != 0);
+        return result.Count(x => x == '1');
+    }
+}
