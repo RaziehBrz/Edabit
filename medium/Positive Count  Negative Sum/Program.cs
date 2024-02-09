a2c45456744c05948690d89c63f021559e169f10
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-09 15:15:13 +0330

    ...

---
+++ medium/Positive Count  Negative Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    static int[] CountPosSumNeg(double[] arr)
+    {
+        return new int[] { arr.Count(x => x > 0), (int)arr.Where(x => x <= 0).Sum() };
+    }
+}
