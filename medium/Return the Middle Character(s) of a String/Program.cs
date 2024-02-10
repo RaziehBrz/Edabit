bb7fcab1204d5c964151b061627d00f13a649208
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-02-10 19:45:14 +0330

    ...

---
+++ medium/Return the Middle Character(s) of a String/Program.cs
@@ -0,0 +1,12 @@
+﻿class Progarm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetMiddle("test"));
+
+    }
+    static string GetMiddle(string str)
+    {
+        return str.Length % 2 == 0 ? str[str.Length / 2 - 1] + str[str.Length / 2].ToString() : str[str.Length / 2].ToString();
+    }
+}
